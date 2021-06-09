943b7c21bc9187bb044d64ed309a3f9baac21965
Félix <felix.potente@alten.es>
2021-06-09 17:18:11 +0200

    Tema 10 Fin video "13. PrimeFlex" , "14. UpperCase, LowerCase y TitleCase Pipes" y "15. Date Pipe"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 
 
 import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    VentasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
