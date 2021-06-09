2bef71b0fc60338e57a1011155a6a0802e052428
Félix <felix.potente@alten.es>
2021-06-09 15:39:51 +0200

    Tema 10 Fin video "7. Prime Button y estilo global" , "8. Cards y botones con íconos" y "9. PrimeNg module"

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+import {CardModule} from 'primeng/card';
+import {ButtonModule} from 'primeng/button';
+
+
+@NgModule({
+ exports:[CardModule,ButtonModule]
+})
+export class PrimeNgModule { }
