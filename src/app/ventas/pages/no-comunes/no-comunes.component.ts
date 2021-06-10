0f34687fd42f791c99862d7bccc6eac6f5cbb6cd
Félix <felix.potente@alten.es>
2021-06-10 12:06:22 +0200

    Tema 10 Fin video "23. Tarea sobre i18nPipes y Primeng" , "24. SlicePipe" , "25. KeyValuePipe" , "26. JsonPipe" y "27. AsyncPipe" FINAL

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -18,12 +19,54 @@ export class NoComunesComponent {
   }
 
   //i18nPlural
-  clientes:string[]=['Félix','Maria', 'Lidia'];
+  clientes:string[]=['Félix','Maria', 'Lidia', 'Marco' , 'Sandra'];
   clientesMapa={
     '=0':'no tenemos ningún cliente esperando.',
     '=1':'tenemos un cliente esperando.',
     '=2':'tenemos 2 clientes esperando.',
     'other': 'tenemos # clientes esperando.'
   }
+
+cambiarCliente(){
+  this.nombre='Blanca';
+  this.genero='femenino';
+}
+
+borrarCliente(){
+this.clientes.pop();
+}
+
+//KeyValue pipe
+persona={
+  nombre:'Félix',
+  edad:35,
+  direccion:'Valladolid, España'
+}
+
+//jsonPipe
+heroes=[
+{
+nombre:'Superman',
+vuela:true
+},{
+nombre:'Robin',
+vuela:false
+},{
+nombre:'Aquaman',
+vuela:false
+}
+]
+
+
+//Async Pipe
+miObservable=interval(5000);
+
+valorPromesa = new Promise((resolve, reject)=>{
+  setTimeout(()=>{
+    resolve('tenemos data de promesa');
+  }, 3500);
+  
+});
+
 
 }
